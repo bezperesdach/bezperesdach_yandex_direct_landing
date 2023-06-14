1d0470717b86d5044024ab854008b40d6101597e
Nikita Maslov <maslomeister@gmail.com>
2023-06-14 06:07:56 +0400

    feat: logo url

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -2,17 +2,18 @@ import { component$ } from "@builder.io/qwik";
 import Logo from "@/components/icons/logo/IconLogo";
 import ToggleTheme from "../common/ToggleTheme";
 import MobileIconLogo from "../icons/logo/mobileIconLogo";
+import { Link } from "@builder.io/qwik-city";
 
 export default component$(() => {
   return (
     <div class="navbar m-auto lg:w-[1024px] px-4 pt-6 sm:px-7 lg:px-0 sm:pt-0">
       <div class="flex-1">
-        <div class="hidden xs:block">
+        <Link href="/" class="hidden xs:block">
           <Logo height={64} />
-        </div>
-        <div class="block xs:hidden">
+        </Link>
+        <Link href="/" class="block xs:hidden">
           <MobileIconLogo height={48} />
-        </div>
+        </Link>
       </div>
       <div class="flex-none">
         <ToggleTheme />
