52a11faa47d3e7407248d04e6105654fb1a54660
Nikita Maslov <maslomeister@gmail.com>
2023-07-09 21:11:02 +0400

    cleanup

--- src/components/common/ToggleTheme.tsx
+++ src/components/common/ToggleTheme.tsx
@@ -36,24 +36,5 @@ export default component$(() => {
         <path d="M21.64,13a1,1,0,0,0-1.05-.14,8.05,8.05,0,0,1-3.37.73A8.15,8.15,0,0,1,9.08,5.49a8.59,8.59,0,0,1,.25-2A1,1,0,0,0,8,2.36,10.14,10.14,0,1,0,22,14.05,1,1,0,0,0,21.64,13Zm-9.5,6.69A8.14,8.14,0,0,1,7.08,5.22v.27A10.15,10.15,0,0,0,17.22,15.63a9.79,9.79,0,0,0,2.1-.22A8.11,8.11,0,0,1,12.14,19.73Z" />
       </svg>
     </label>
-    // <button
-    //   type="button"
-    //   class="text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 rounded-lg text-sm p-2.5 inline-flex items-center"
-    //   aria-label="Toggle between Dark and Light mode"
-    //   onClick$={() => {
-    //     switch (store.theme) {
-    //       case "dark":
-    //         document.documentElement.setAttribute("data-theme", "light");
-    //         store.theme = window.localStorage.theme = "light";
-    //         break;
-    //       default:
-    //         document.documentElement.setAttribute("data-theme", "dark");
-    //         store.theme = window.localStorage.theme = "dark";
-    //         break;
-    //     }
-    //   }}
-    // >
-    //   {store.theme == "dark" ? <IconMoon class={iconClass} /> : <IconSun class={iconClass} />}
-    // </button>
   );
 });
